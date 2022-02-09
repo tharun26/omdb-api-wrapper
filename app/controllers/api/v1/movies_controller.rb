7fb06729229aed560a54b2f37674793b48474a92
tharun26 <tharun26@gmail.com>
2022-02-09 10:23:37 +0530

    Implementing controllers

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,20 +1,24 @@
 module Api
-    module V1
-      class MoviesController < ApplicationController
-        def search_by_id
-        end
-    
-        def multiple_search
-
-        end
-  
-        private
-          def permitted_params_search_by_id
-          end
-
-          def permitted_params_multiple_search
-          end
+  module V1
+    class MoviesController < ApplicationController
+      def search_by_id
+        response, status = Omdb::Handler.by_id(permitted_params_search_by_id)
+        json_response(response, status)
       end
+  
+      def multiple_search
+        response, status = Omdb::Handler.multiple_search(permitted_params_multiple_search)
+        json_response(response, status)
+      end
+
+      private
+        def permitted_params_search_by_id
+          params.permit(:id)
+        end
+
+        def permitted_params_multiple_search
+          params.permit(:title, :type, :year, :page)
+        end
     end
   end
-  
+end
