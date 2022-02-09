447c920a64b4ebae8111f02b7a2f826036e8869a
tharun26 <tharun26@gmail.com>
2022-02-09 10:14:36 +0530

    Adding Movies Controller

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,20 @@
+module Api
+    module V1
+      class MoviesController < ApplicationController
+        def search_by_id
+        end
+    
+        def multiple_search
+
+        end
+  
+        private
+          def permitted_params_search_by_id
+          end
+
+          def permitted_params_multiple_search
+          end
+      end
+    end
+  end
+  
---
+++ app/controllers/response.rb
@@ -0,0 +1,5 @@
+module Response
+    def json_response(data, status = :ok)
+      render json: data, status: status
+    end
+  end  
