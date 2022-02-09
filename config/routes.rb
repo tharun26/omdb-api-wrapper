96e866897dab91866b546e3b72343f577eaa5ecd
tharun26 <tharun26@gmail.com>
2022-02-09 10:15:27 +0530

    Adding routes for the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/movies/:id', to: 'movies#search_by_id', as: :movie
+      get '/search', to: 'movies#multiple_search'
+    end
+  end
 end
